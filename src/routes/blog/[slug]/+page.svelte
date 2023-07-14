c87ae0fdec71257a3f9f77d1da53a4dd067037d8
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-07-14 18:27:37 +0000

    Working on slug page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
 
-	export let data;
+	export let data: PageData;
 </script>
 
-<svelte />
+<div class="px-4 py-[72px] mb-[72px] bg-gray-100">
+	<h1>{data.metadata.title}</h1>
+</div>
+
+<article class="max-w-prose mx-auto px-4">
+	<svelte:component this={data.component} />
+</article>
--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -3,10 +3,11 @@ import type { PageLoad } from "./$types";
 export const load = (async ({ params }) => {
 	const { slug } = params;
 	const article = await import(`../content/${slug}/index.md`);
+	const component = article.default;
 	const metadata: ArticleMeta = article.metadata;
 
 	return {
-		article,
+		component,
 		metadata
 	};
 }) satisfies PageLoad;
