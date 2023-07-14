2714998c0d9894697547f8e51a3abc8a38d420b1
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-07-14 22:35:20 +0000

    Working on slug page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
-
+	import CalendarIcon from "$icons/calendar.svg?component";
+	import { formatDate } from "$utils";
 	export let data: PageData;
 </script>
 
 <div class="px-4 py-[72px] mb-[72px] bg-gray-100">
 	<h1>{data.metadata.title}</h1>
+	<ul class="text-sm mt-[48px]">
+		<li class="flex items-center gap-2">
+			<CalendarIcon width="16" height="16" />
+			<time datetime={data.metadata.date.toString()}>{formatDate(data.metadata.date)}</time>
+		</li>
+	</ul>
 </div>
 
 <article class="max-w-prose mx-auto px-4">
