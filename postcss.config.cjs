f0c7ae51f4af147c5d3dc30d0af991e86274c30b
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-07-14 18:15:01 +0000

    Installed husky and postcss presetenv

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,11 @@
 const tailwindcss = require("tailwindcss");
+const presetEnv = require("postcss-preset-env");
 
 const config = {
 	plugins: [
+		presetEnv({
+			stage: 1
+		}),
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss()
 		//But others, like autoprefixer, need to run after,
