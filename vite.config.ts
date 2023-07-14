fdeb5df9b9e54ef89445f132b6254deb57a34788
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-07-14 18:44:00 +0000

    Installed sveltekit Svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import svelteSvg from "@poppanator/sveltekit-svg";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), svelteSvg()],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
 	}
