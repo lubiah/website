28a640eb333f02a4a72aa20066713965fac7b59b
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-07-18 16:20:17 +0000

    Added more metadata

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
-	import Card from "./components/BlogCard";
+	import Card from "$components/BlogCard";
+	import Head from "svelte-seo";
 
 	export let data: PageData;
+
+	const SEO = {
+		title: `The Garden - My thoughts and notes • Lubiah`,
+		description:
+			"The homepage of all my thoughts, notes, and findings on programming and other topics that I find interesting"
+	};
 </script>
 
+<Head title={SEO.title} description={SEO.description} />
+
 <main class="px-4 max-w-screen-laptop mx-auto">
-	<div class="my-[72px] tablet:my-[84px] laptop:my-[96px]">
+	<div class="my-18 tablet:my-[84px] laptop:my-[96px]">
 		<h1 class="my-0 mb-4">The Garden</h1>
 		<p class="text-gray-700 py-0">
 			My thoughts and notes on programming and other topics which I find interesting.
@@ -16,7 +25,7 @@
 		<ul class="flex gap-x-4 mb-[24px] tablet:mb-[28px]">
 			{#each data.tags as tag}
 				<a
-					href="/blog/tag/{tag}"
+					href="/blog/tags/{tag}"
 					class="text-base p-2 border border-gray-300 leading-none rounded-sm">{tag}</a
 				>
 			{/each}
