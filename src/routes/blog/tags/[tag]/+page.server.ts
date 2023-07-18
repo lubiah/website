4d3bc649981709a98a9139dedf8153762408be11
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-07-18 17:17:34 +0000

    Updated tags page

--- src/routes/blog/tags/[tag]/+page.server.ts
+++ src/routes/blog/tags/[tag]/+page.server.ts
@@ -1,11 +1,13 @@
-import { getArticlesMeta } from "../..";
+import { getArticlesMeta, getTags } from "../..";
 import type { PageServerLoad } from "./$types";
 
 export const load = (({ params }) => {
 	const { tag } = params;
+	const tags = getTags();
 	const articles = getArticlesMeta().filter((article) => article.tags.includes(tag));
 	return {
 		articles,
-		tag
+		tag,
+		tags
 	};
 }) satisfies PageServerLoad;
--- src/routes/blog/tags/[tag]/+page.svelte
+++ src/routes/blog/tags/[tag]/+page.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
 	import type { PageServerData } from "./$types";
 	import Card from "$components/BlogCard";
+	import Head from "svelte-seo";
 
 	export let data: PageServerData;
 </script>
 
-<main class="max-w-screen-laptop mx-auto">
-	<div class="my-18">
+<Head noindex={true} nofollow={true} />
+
+<main class="max-w-screen-laptop mx-auto px-4">
+	<div class="my-18 tablet:my-21 laptop:my-24">
 		<a href="/blog" class="text-gray-500 font-semibold">The Garden</a>
-		<h1 class="my-0">Content under `{data.tag}`</h1>
+		<h1 class="my-0 mt-2">Content under `{data.tag}`</h1>
 	</div>
+	<ul class="flex gap-x-4 mb-6 tablet:mb-7 laptop:mb-8">
+		{#each data.tags as tag}
+			<a
+				href={tag === data.tag ? "/blog" : `/blog/tags/${tag}`}
+				class:active={tag === data.tag}
+				class="text-base p-2 border border-gray-300 leading-none rounded-sm">{tag}</a
+			>
+		{/each}
+	</ul>
 	<div>
 		<div class="flex flex-col gap-y-12 laptop:grid laptop:grid-cols-2 laptop:gap-x-12">
 			{#each data.articles as article}
@@ -24,3 +36,9 @@
 		</div>
 	</div>
 </main>
+
+<style lang="postcss">
+	.active {
+		@apply bg-gray-800 text-gray-300 border-none;
+	}
+</style>
