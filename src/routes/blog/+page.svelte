7b113db26e93dbf0c7622c6b0e4a3d63f45c5cc3
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-07-18 07:59:29 +0000

    Updated blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
+	import Card from "./components/BlogCard";
 
 	export let data: PageData;
 </script>
 
-{#each data.articles as article}
-	<li>
-		<a href="/blog/{article.slug}">{article.title}</a>
-	</li>
-{/each}
+<main class="px-4 max-w-screen-laptop mx-auto">
+	<div class="my-[72px] tablet:my-[84px] laptop:my-[96px]">
+		<h1 class="my-0 mb-4">The Garden</h1>
+		<p class="text-gray-700 py-0">
+			My thoughts and notes on programming and other topics which I find interesting.
+		</p>
+	</div>
+	<div>
+		<ul class="flex gap-x-4 mb-[24px] tablet:mb-[28px]">
+			{#each data.tags as tag}
+				<a
+					href="/blog/tag/{tag}"
+					class="text-base p-2 border border-gray-300 leading-none rounded-sm">{tag}</a
+				>
+			{/each}
+		</ul>
+		<div class="flex flex-col gap-y-12 laptop:grid laptop:grid-cols-2 laptop:gap-x-12">
+			{#each data.articles as article}
+				<Card
+					title={article.title}
+					excerpt={article.excerpt}
+					date={article.date}
+					slug={article.slug}
+					readingTime={article.readingTime.text}
+				/>
+			{/each}
+		</div>
+	</div>
+</main>
