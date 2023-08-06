00e6f80d0c9117e30fed99a0afc0540536d63228
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-08-06 17:10:14 +0000

    Working on styles

--- src/routes/blog/[slug]/components/toc/index.svelte
+++ src/routes/blog/[slug]/components/toc/index.svelte
@@ -2,9 +2,9 @@
 	export let headings: ArticleMeta["headings"];
 </script>
 
-<ul>
+<ul class="pl-0">
 	{#each headings as { value, id }}
-		<li class="p-1 mb-1 rounded-xs toc_li hover:bg-gray-100">
+		<li class="list-marker-none p-1 mb-1 rounded-xs toc_li hover:bg-gray-100">
 			<a class="block toc_link" href="#{id}">{value}</a>
 		</li>
 	{/each}
