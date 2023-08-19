2f3d0343f8a273745b929408146a5062c2ff1ab8
Lucretius Biah <lucretiusbiah@protonmail.com>
2023-08-19 23:08:48 +0000

    Remove patch and add snytax highlighting

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,4 +1,5 @@
 import { defineMDSveXConfig as defineConfig } from "mdsvex";
+import { fileURLToPath } from "node:url";
 
 import readingTime from "mdsvex-reading-time";
 import remarkEmoji from "remark-emoji";
@@ -6,16 +7,17 @@ import remarkHeadings from "@sveltinio/remark-headings";
 import remarkDirective from "remark-directive";
 import remarkAsides from "./remarkPlugins/asides/index.js";
 import remarkFrontmatter from "remark-frontmatter";
-import remarkHighlighter from "remark-highlighter";
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
 import remarkParse from "remark-parse";
 
 import rehypeSlug from "rehype-slug";
+import rehypeHighlighter from "rehype-highlighter";
+import rehypeEscapeCode from "@lubiah/rehype-escape-code";
 
 const config = defineConfig({
 	extensions: [".md"],
-
+	highlight: false,
 	smartypants: {
 		dashes: "oldschool"
 	},
@@ -28,20 +30,30 @@ const config = defineConfig({
 		remarkGfm,
 		remarkBreaks,
 		remarkEmoji,
-		[
-			remarkHighlighter,
-			{
-				theme: "dracula",
-				inlineCode: {
-					spaceSubstitution: true
-				}
-			}
-		],
 		[remarkEmoji, { accessible: true }],
 		remarkHeadings,
 		remarkAsides
 	],
-	rehypePlugins: [rehypeSlug]
+	rehypePlugins: [
+		rehypeSlug,
+
+		[
+			rehypeHighlighter,
+			{
+				theme: "dracula",
+				loadThemes: {
+					"dracula-light": fileURLToPath(
+						new URL("./misc/themes/dracula-dot-min-white.json", import.meta.url)
+					)
+				},
+				inlineCode: {
+					theme: "dracula-light",
+					spaceSubstitution: true
+				}
+			}
+		],
+		rehypeEscapeCode
+	]
 });
 
 export default config;
